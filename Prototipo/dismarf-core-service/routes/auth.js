f331e11e24fd2be5a3e3dbedafbdffd576d296b0
Daniels <135479855+StudiosDanilIs@users.noreply.github.com>
2026-05-29 06:00:38 -0400

    remode proyecto

--- Prototipo/dismarf-core-service/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Rutas públicas (no requieren token)
-router.post('/register', authController.registrarUsuario);
-router.post('/login', authController.loginUsuario);
-
-module.exports = router;
